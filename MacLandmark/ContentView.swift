b4efb380f8331f9cdf3138da17ee7e858bd10b77
andiy <andiyuniar@live.com>
2022-03-27 21:39:32 +0700

    Creating mac OS app - section 1 & 2

---
+++ MacLandmark/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmark
+//
+//  Created by Andi Yuniarto on 26/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
