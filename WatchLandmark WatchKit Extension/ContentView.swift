845cef466942d384b47c1267a5275c0f4cec3a8d
andiy <andiyuniar@live.com>
2022-03-28 20:17:29 +0700

    develop ios watch section 1 & 2

---
+++ WatchLandmark WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmark WatchKit Extension
+//
+//  Created by Andi Yuniarto on 28/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchLandmark WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmark WatchKit Extension
+//
+//  Created by Andi Yuniarto on 28/03/22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
